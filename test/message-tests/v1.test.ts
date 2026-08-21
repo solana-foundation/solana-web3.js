fade28ebe93c8ddc554ba116d792bceebfa61912
amilz <85324096+amilz@users.noreply.github.com>
2026-08-21 05:27:09 -0700

    test: port v1 message decode cases from kit
    https://github.com/anza-xyz/kit/tree/main/packages/transaction-messages/src/codecs/v1/__tests__

--- test/message-tests/v1.test.ts
+++ test/message-tests/v1.test.ts
@@ -7,6 +7,31 @@ function createTestKeys(count: number): Array<PublicKey> {
   return new Array(count).fill(0).map(() => PublicKey.unique());
 }
 
+const TEST_BLOCKHASH_BYTES = new Array(32).fill(10);
+const TEST_BLOCKHASH = 'gBxS1f6uyyGPuW5MzGBukidSb71jdsCb5fZaoSzULE5';
+const TEST_KEY_1_BYTES = new Array(32).fill(11);
+const TEST_KEY_1 = new PublicKey('k7FaK87WHGVXzkaoHb7CdVPgkKDQhZ29VLDeBVbDfYn');
+const TEST_KEY_2_BYTES = new Array(32).fill(12);
+const TEST_KEY_2 = new PublicKey('p2Yicb86aZig616Eav2VWG9vuXR5mEqhtzshZYBxzsV');
+const TEST_KEY_3_BYTES = new Array(32).fill(13);
+const TEST_KEY_3 = new PublicKey('swqrv48gsrwpBFbftEwnP2vB4jckpvfGJfXkwaniLCC');
+
+function serializeConfigTestMessage(
+  configMask: number,
+  configValueBytes: Array<number>,
+): Uint8Array {
+  return new Uint8Array([
+    0x81,
+    ...[1, 0, 0],
+    ...[configMask, 0, 0, 0],
+    ...TEST_BLOCKHASH_BYTES,
+    0,
+    1,
+    ...TEST_KEY_1_BYTES,
+    ...configValueBytes,
+  ]);
+}
+
 describe('MessageV1', () => {
   it('version', () => {
     const message = new MessageV1({
@@ -101,11 +126,202 @@ describe('MessageV1', () => {
     });
   });
 
+  it('deserialize (no config values)', () => {
+    const serializedMessage = new Uint8Array([
+      0x81,
+      ...[2, 1, 1],
+      ...[0, 0, 0, 0],
+      ...TEST_BLOCKHASH_BYTES,
+      1,
+      2,
+      ...TEST_KEY_1_BYTES,
+      ...TEST_KEY_2_BYTES,
+      ...[1, 1, 3, 0],
+      ...[0, 1, 2, 3],
+    ]);
+    const message = MessageV1.deserialize(serializedMessage);
+    expect(message.version).to.eq(1);
+    expect(message.header).to.eql({
+      numRequiredSignatures: 2,
+      numReadonlySignedAccounts: 1,
+      numReadonlyUnsignedAccounts: 1,
+    });
+    expect(message.recentBlockhash).to.eq(TEST_BLOCKHASH);
+    expect(message.staticAccountKeys).to.eql([TEST_KEY_1, TEST_KEY_2]);
+    expect(message.compiledInstructions).to.eql([
+      {
+        programIdIndex: 1,
+        accountKeyIndexes: [0],
+        data: new Uint8Array([1, 2, 3]),
+      },
+    ]);
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: null,
+      heapSize: null,
+      loadedAccountsDataSizeLimit: null,
+      priorityFee: null,
+    });
+  });
+
+  it('deserialize (multiple instructions)', () => {
+    const serializedMessage = new Uint8Array([
+      0x81,
+      ...[1, 0, 2],
+      ...[0, 0, 0, 0],
+      ...TEST_BLOCKHASH_BYTES,
+      2,
+      3,
+      ...TEST_KEY_1_BYTES,
+      ...TEST_KEY_2_BYTES,
+      ...TEST_KEY_3_BYTES,
+      ...[1, 2, 3, 0],
+      ...[2, 1, 0, 0],
+      ...[0, 2, 10, 20, 30],
+      ...[0],
+    ]);
+    const message = MessageV1.deserialize(serializedMessage);
+    expect(message.staticAccountKeys).to.eql([
+      TEST_KEY_1,
+      TEST_KEY_2,
+      TEST_KEY_3,
+    ]);
+    expect(message.compiledInstructions).to.eql([
+      {
+        programIdIndex: 1,
+        accountKeyIndexes: [0, 2],
+        data: new Uint8Array([10, 20, 30]),
+      },
+      {
+        programIdIndex: 2,
+        accountKeyIndexes: [0],
+        data: new Uint8Array([]),
+      },
+    ]);
+  });
+
+  it('deserialize transactionConfig (priority fee only)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(0b00011, [136, 19, 0, 0, 0, 0, 0, 0]),
+    );
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: null,
+      heapSize: null,
+      loadedAccountsDataSizeLimit: null,
+      priorityFee: 5000,
+    });
+  });
+
+  it('deserialize transactionConfig (large priority fee)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(
+        0b00011,
+        [255, 255, 255, 255, 255, 255, 31, 0],
+      ),
+    );
+    expect(message.transactionConfig?.priorityFee).to.eq(
+      Number.MAX_SAFE_INTEGER,
+    );
+  });
+
+  it('deserialize transactionConfig (compute unit limit only)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(0b00100, [64, 13, 3, 0]),
+    );
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: 200000,
+      heapSize: null,
+      loadedAccountsDataSizeLimit: null,
+      priorityFee: null,
+    });
+  });
+
+  it('deserialize transactionConfig (loaded accounts data size limit only)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(0b01000, [0, 250, 0, 0]),
+    );
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: null,
+      heapSize: null,
+      loadedAccountsDataSizeLimit: 64000,
+      priorityFee: null,
+    });
+  });
+
+  it('deserialize transactionConfig (heap size only)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(0b10000, [0, 232, 3, 0]),
+    );
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: null,
+      heapSize: 256000,
+      loadedAccountsDataSizeLimit: null,
+      priorityFee: null,
+    });
+  });
+
+  it('deserialize transactionConfig (multiple values)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(
+        0b01011,
+        [136, 19, 0, 0, 0, 0, 0, 0, 0, 250, 0, 0],
+      ),
+    );
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: null,
+      heapSize: null,
+      loadedAccountsDataSizeLimit: 64000,
+      priorityFee: 5000,
+    });
+  });
+
+  it('deserialize transactionConfig (all values)', () => {
+    const message = MessageV1.deserialize(
+      serializeConfigTestMessage(
+        0b11111,
+        [136, 19, 0, 0, 0, 0, 0, 0, 64, 13, 3, 0, 0, 250, 0, 0, 0, 232, 3, 0],
+      ),
+    );
+    expect(message.transactionConfig).to.eql({
+      computeUnitLimit: 200000,
+      heapSize: 256000,
+      loadedAccountsDataSizeLimit: 64000,
+      priorityFee: 5000,
+    });
+  });
+
+  it('deserialize failure (missing config value bytes)', () => {
+    expect(() =>
+      MessageV1.deserialize(serializeConfigTestMessage(0b00100, [64, 13])),
+    ).to.throw('Reached end of buffer unexpectedly');
+  });
+
+  it('deserialize failure (trailing bytes)', () => {
+    expect(() =>
+      MessageV1.deserialize(serializeConfigTestMessage(0, [0])),
+    ).to.throw(
+      'Expected no bytes to remain after deserializing a version 1 message',
+    );
+  });
+
   it('deserialize failure (invalid priority fee bits)', () => {
     const serializedMessage = new Uint8Array([
       0x81,
       ...[1, 0, 0],
       ...[0b00001, 0, 0, 0],
+      ...new Array(32).fill(0),
+      0,
+      0,
+    ]);
+    expect(() => MessageV1.deserialize(serializedMessage)).to.throw(
+      'Expected both or neither of the priority fee bits to be set in the transaction config mask',
+    );
+  });
+
+  it('deserialize failure (other invalid priority fee bit)', () => {
+    const serializedMessage = new Uint8Array([
+      0x81,
+      ...[1, 0, 0],
+      ...[0b00010, 0, 0, 0],
       ...new Array(32).fill(0),
       0,
       0,
