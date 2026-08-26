e9190bc8c5e54d86681119fa6b032ea50909df88
amilz <85324096+amilz@users.noreply.github.com>
2026-08-26 15:18:04 -0700

    chore: add v1 size constant
    adds const (sibling to PACKET_DATA_SIZE)

--- src/transaction/constants.ts
+++ src/transaction/constants.ts
@@ -12,3 +12,8 @@ export const VERSION_PREFIX_MASK = 0x7f;
 export const SIGNATURE_LENGTH_IN_BYTES = 64;
 
 export const VERSION_1_MESSAGE_PREFIX = 0x81;
+
+/**
+ * The maximum size of a version 1 transaction in bytes.
+ */
+export const V1_TRANSACTION_SIZE_LIMIT = 4096;
